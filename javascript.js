a6ac34af1b682c34e1f0bc7ca525a07332c009ed
Lukas S <114870613+Nympherr@users.noreply.github.com>
2023-02-06 17:32:08 +0200

    Add play one game functionality

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let player;
 let computer;
+let result;
 let play = false;
 
 function computerChoice(computer){
@@ -40,10 +41,52 @@ function playerChoice(player){
     return player;
 }
 
-function playRound(){
+function playRound(player, computer){
 
+        console.log(player);
+        console.log(computer);
+    switch(player){
+
+        case "rock":
+            if(computer == "rock"){
+                console.log("It's a tie! rock vs rock")
+            }
+            else if(computer == "paper"){
+                console.log("Player loses! rock vs paper")
+            }
+            else {
+                console.log("Player wins! rock vs scissors")
+            }
+            break;
+        
+        case "paper":
+            if(computer == "rock"){
+                console.log("Player wins! paper wins against rock")
+            }
+            else if(computer == "paper"){
+                console.log("It's a tie! paper does not beat paper")
+            }
+            else {
+                console.log("Computer wins! paper loses against scissors")
+            }
+            break;
+        
+        case "scissors":
+            if(computer == "rock"){
+                console.log("Computer wins! scissors lose against rock")
+            }
+            else if(computer == "paper"){
+                console.log("Player wins! scissors win against paper")
+            }
+            else {
+                console.log("It's a tie! scissors don't beat scissors")
+            }
+            break;
+    }
 }
-
+computer = computerChoice(computer);
+player = playerChoice(player);
+playRound(player,computer);
 function game(){
 
 }
